ab65eb01ec67ebb1efd9f5f9407b1531d4b3972a
hashansampath377-coder <hashansampath377@gmail.com>
2026-04-26 10:45:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: '𝐋𝐀𝐒𝐄𝐑 𝐌𝐃',
+  packname: 'Panda 𝐌𝐃',
   author: '‎',
-  botName: "𝐋𝐀𝐒𝐄𝐑 𝐌𝐃",
-  botOwner: '𝙳𝙴𝚅𝚆𝙴𝙴𝙳', // Your name
-  ownerNumber: '50934410653', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Panda 𝐌𝐃",
+  botOwner: 'Hashan', // Your name
+  ownerNumber: '94782543893', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.9",
-  updateZipUrl: "https://github.com/TECHGOAT333/LASER-MD/archive/refs/heads/main.zip'",
+  updateZipUrl: "https://github.com/hashansampath377-coder/LASER-MD/archive/refs/heads/main.zip'",
 };
 
 module.exports = settings;
